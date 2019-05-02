ff381534c88ab3ad5a567a8cbae58cc995a7786e
Jhia Turner <jhimitu@gmail.com>
2019-05-02 13:02:18 -0700

    create single function for db and api calls

--- schema.sql
+++ schema.sql
@@ -1,7 +1,23 @@
 DROP TABLE IF EXISTS location;
+DROP TABLE IF EXISTS weather;
+DROP TABLE IF EXISTS events;
+
 CREATE TABLE location (
   latitude DECIMAL,
   longitude DECIMAL,
   formatted_query VARCHAR(255),
   search_query VARCHAR(255)
 );
+
+CREATE TABLE weather (
+  formatted_query VARCHAR(255),
+  summary VARCHAR(512),
+  time VARCHAR(255)
+);
+
+CREATE TABLE events (
+  link VARCHAR(255),
+  name VARCHAR(255),
+  event_date VARCHAR(255),
+  summary VARCHAR(512)
+)
--- server.js
+++ server.js
@@ -18,70 +18,88 @@ app.get('/hello', (request, response) => {
 });
 
 app.get('/location', (request, response) => {
-  getLocation(request.query.data)
+  let sqlStatement = 'SELECT * FROM location WHERE search_query = $1;';
+  let sqlInsertStatement = 'INSERT INTO location (search_query, formatted_query, latitude, longitude) VALUES ( $1, $2, $3, $4);';
+  let geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
+  console.log('Location: ',geocodeURL);
+
+  getData(request.query.data, sqlStatement, sqlInsertStatement, geocodeURL, Location)
     .then(location => response.send(location))
     .catch(error => handleError(error, response));
-
-
-  // const queryData = request.query.data;
-  // let geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${queryData}&key=${process.env.GEOCODE_API_KEY}`;
-  // superagent.get(geocodeURL)
-  //   .end((err, res) => {
-  //     if (err && err.status !== 200) {
-  //       const errorResponse500 = {'status': 500, 'responseText': 'Sorry, something went wrong' };
-
-  //       response.status(500).send(errorResponse500);
-  //     } else {
-  //       const location = new Location(queryData, res);
-  //       response.status(200).send(location);
-  //     }
-  //   });
 });
 
 function handleError(error, response) {
-  console.log(error);
+  //console.log(error);
   if (response) response.status(500).send('Sorry, something went wrong');
 }
 
-function getLocation(query) {
-  let sqlStatement = 'SELECT * FROM location WHERE search_query = $1;';
+function getData(query, sqlStatement, sqlInsertStatement, apiURL, Constructor) {
   let values = [ query ];
   return client.query(sqlStatement, values)
     .then((data) => {
       if (data.rowCount > 0) {
-        console.log('DB DATA: ', data.rows[0]);
-        return data.rows[0];
+        // Somne fuction for databse
+        return recordsExists(data);
       } else {
-        let geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
-        return superagent.get(geocodeURL)
-          .then(res => {
-            console.log('API DATA', res);
-            let tempLocation = new Location(query, res);
-            let sqlInsertStatement = 'INSERT INTO location (search_query, formatted_query, latitude, longitude) VALUES ( $1, $2, $3, $4);';
-            let insertValues = [ tempLocation.search_query, tempLocation.formatted_query, tempLocation.latitude, tempLocation.longitude ];
-            client.query(sqlInsertStatement, insertValues);
-            return tempLocation;
-          })
-          .catch(err => handleError(err));
+        return recordsDontExist(query, sqlInsertStatement, apiURL, Constructor);
       }
     });
+}
+
+// Function for if records exist
+function recordsExists(data){
+  // Test case for location
+  console.log('DB DATA: ', data.rows[0]);
+  if(data.rows.length === 1) return data.rows[0];
+  else return data.rows[0];
+}
+
+// Functions for if records don't exist
+function recordsDontExist(query, sqlInsertStatement, apiURL, Constructor){
+  return superagent.get(apiURL)
+    .then(res => {
+      //console.log('API DATA', res.body);
+      // Some fuction for API
+      let tempObject = new Constructor(query, res);
+      console.log(tempObject);
+
+      let objectLength = 0;
+      // Set lengths based on consutructor
+      console.log(Constructor);
+      if(Constructor === Weather){
+        objectLength = tempObject.dailyForecast.length;
+      }
+      if(Constructor === Location){
+        objectLength = 1;
+      }
+      console.log(objectLength);
+      for(let i = 0; i < objectLength; i++){
+        let insertValues;
+        if(Constructor === Weather){
+          insertValues = Object.values(tempObject.dailyForecast[i]);
+          insertValues.unshift(query.formatted_query);
+        }else if(Constructor === Location){
+          insertValues = Object.values(tempObject);
+          console.log(insertValues);
+        }
+        console.log('values to put in DB', insertValues);
+        client.query(sqlInsertStatement, insertValues);
+        console.log('inserted to DB');
+      }
+      return tempObject;
+    })
+    .catch(err => handleError(err));
 }
 
 app.get('/weather', (request, response) => {
   const lat = request.query.data.latitude;
   const lng = request.query.data.longitude;
+  let sqlStatement = 'SELECT summary, time FROM weather WHERE formatted_query = $1;';
+  let sqlInsertStatement = 'INSERT INTO weather(formatted_query, summary, time) VALUES ( $1, $2, $3);';
   const weatherURL =`https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${lat},${lng}`;
-  superagent.get(weatherURL)
-    .end((err, res) => {
-      if (err && err.status !== 200) {
-        const errorResponse500 = {'status': 500, 'responseText': 'Sorry, something went wrong' };
-
-        response.status(500).send(errorResponse500);
-      } else {
-        const weather = new Weather(res);
-        response.status(200).send(weather.dailyForecast);
-      }
-    });
+  getData(request.query.data, sqlStatement, sqlInsertStatement, weatherURL, Weather)
+    .then(location => response.send(location))
+    .catch(error => handleError(error, response));
 });
 
 app.get('/events', (request, response) => {
@@ -117,7 +135,7 @@ const Location = function(searchQuery, jsonData) {
   this.longitude = longitude;
 };
 
-const Weather = function(jsonData) {
+const Weather = function(query, jsonData) {
   this.dailyForecast = [...jsonData.body.daily.data].map(forecast => {
     const summary = forecast['summary'];
     const time = (new Date(forecast['time'] * 1000)).toDateString();
